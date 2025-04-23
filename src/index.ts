30e2e5c7cfed44e632c74ddd9b74d7102bbd4b2f
Nico <nicolasaddamo1@gmail.com>
2025-04-23 12:45:13 -0300

    feat: added endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+import { log } from 'console';
 import express from 'express'
 import { Request, Response } from 'express'
-import { isNamespaceExport } from 'typescript';
 
 const app = express()
 const PORT = process.env.PORT || 3000;
@@ -22,3 +22,57 @@ app.use((req, res, next) => {
     next()
 });
 
+app.get('/status', (req: Request, res: Response) => {
+    if (selectedSystem) {
+        const systems = Object.keys(DAMAGED_SYSTEM);
+        const randomIndex = Math.floor(Math.random() * systems.length);
+        selectedSystem = systems[randomIndex];
+    }
+    console.log(`Reporting damaged system: ${selectedSystem}`);
+    res.json({
+        damaged_system: selectedSystem
+    });
+});
+
+app.get('/repair-bay', (req: Request, res: Response) => {
+    if (selectedSystem) {
+        const systems = Object.keys(DAMAGED_SYSTEM);
+        const randomIndex = Math.floor(Math.random() * systems.length);
+        selectedSystem = systems[randomIndex];
+    }
+
+    const repairCode = DAMAGED_SYSTEM[selectedSystem]
+
+    const htmlContent = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Repair</title>
+</head>
+<body>
+    <div class="anchor-point>${repairCode} for system: ${selectedSystem} </div>
+</body>
+</html>
+`;
+
+
+    console.log(`Repair bay requested. Showing code: ${repairCode} for system: ${selectedSystem}`);
+
+
+    res.setHeader('Content-Type', 'text/html');
+    res.send(htmlContent);
+});
+
+app.post('/teapot', (req: Request, res: Response) => {
+    console.log('I\' m a teapot!');
+    res.status(418).send("I'm a teapot!");
+});
+
+app.listen(PORT, () => {
+    console.log('Space Rescye API is running on port ' + PORT);
+    console.log('Endpoints available:');
+    console.log('- GET /status - Reports damaged system');
+    console.log('- GET /repair-bay');
+    console.log();
+}
+);
